8e45c37d61fdb0f6387500cdea3c5e41b2ee2c16
Raphael Vigneault Ð. <triangle1232009@hotmail.com>
2025-07-18 14:33:55 -0400

    init

---
+++ nodes/Viridem/Viridem.node.ts
@@ -0,0 +1,38 @@
+import { INodeType, INodeTypeDescription, NodeConnectionType } from 'n8n-workflow';
+import { N8NPropertiesBuilder, N8NPropertiesBuilderConfig } from '@devlikeapro/n8n-openapi-node';
+import * as doc from './openapi.json';
+
+const config: N8NPropertiesBuilderConfig = {}
+const parser = new N8NPropertiesBuilder(doc, config);
+const properties = parser.build()
+
+export class Viridem implements INodeType {
+    description: INodeTypeDescription = {
+        displayName: 'Viridem',
+        name: 'viridem',
+        icon: 'file:logo.svg',
+        group: ['transform'],
+        version: 1,
+        subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+        description: 'Interact with Viridem API',
+        defaults: {
+            name: 'Viridem',
+        },
+        inputs: [NodeConnectionType.Main],
+        outputs: [NodeConnectionType.Main],
+        credentials: [
+            {
+                name: 'viridemApi',
+                required: false,
+            },
+        ],
+        requestDefaults: {
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            baseURL: '={{$credentials.url}}',
+        },
+        properties: properties, // <==== HERE
+    };
+}
